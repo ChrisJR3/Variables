096311fb507f5b634d08242dc71b33f117dc860f
ChrisJR3 <43279462+ChrisJR3@users.noreply.github.com>
2018-09-25 19:37:26 -0400

    Hey again, try this.

--- Variables/Form1.cs
+++ Variables/Form1.cs
@@ -42,6 +42,8 @@ namespace Variables
 
         private void button2_Click(object sender, EventArgs e)
         {
+            // Wayne Gretzky
+            
             button1.Visible = false;
             button2.Visible = false;
             button3.Visible = false;
@@ -50,17 +52,18 @@ namespace Variables
             string.Pi = "3.14";
             string.r = "r";
             string.A = "A";
+            // Not sure about this
             string.outputLabel.Text = Pi;
             string.outputLabel.Text = r;
             string.outputLabel.Text = A;
-            string.outputLabel.Text = ^2;
+            string.exponent1 = ^2;
             int radius = 15;
             int radius/exponent = 225;
-            int radius/Exponent/Pi = 706.5;
-            string.outputLabel.Text = A + " = " + Pi + r + Exponent;
-            string.outputLabel.Text = n/A + " = " + Pi + radius + Exponent;
+            int radius/exponent/Pi = 706.5;
+            string.outputLabel.Text = A + " = " + Pi + r + exponent1;
+            string.outputLabel.Text = n/A + " = " + Pi + radius + exponent1;
             string.outputLabel.Text = n / A + " = " + Pi + radius / exponent;
-            string.outputLabel.Text = n / A + " = " + radius / Exponent / Pi;
+            string.outputLabel.Text = n / A + " = " + radius / exponent / Pi;
             string.outputLabel.Text = n / "Therefore" + A + "is 706.5.";
 
             Thread.Sleep(5000);
@@ -74,23 +77,23 @@ namespace Variables
 
         private void button3_Click(object sender, EventArgs e)
         {
+            Area
+            
             button1.Visible = false;
             button2.Visible = false;
             button3.Visible = false;
             button4.Visible = false;
 
-            string.Pi = "3.14";
-            string.r = "r";
-            string.Cost = "Cost";
-            string.outputLabel.Text = Pi;
-            string.outputLabel.Text = r;
-            string.outputLabel.Text = A;
-            string.outputLabel.Text = ^ 2;
-            int Width = 15;
-            int Length = 225;
-            int Width/Length= 706.5;
-            string.outputLabel.Text = Cost + " = " + 19.95 + "(" + Width +'m' + Length + "m)";
-            string.outputLabel.Text = n / "Therefore" + A + "is 706.5.";
+            string.cost1 = "Cost";
+            int width1 = 8.5;
+            int length1 = 6;
+            int price1 = 19.95;
+            int width1/length1 = 51;
+            int width1/length1/price1 = 1017.45;
+            string.outputLabel.Text = cost1 + " = " + price1 + "(" + width1 +'m' + " + " + length1 + "m)";
+            string.outputLabel.Text = n/cost1 + " = " + price1 + width1/length1 + "m";
+            string.outputLabel.Text = n/cost1 + " = " + width1/length1/price1;
+            string.outputLabel.Text = n / "Therefore" + cost1 + "is" + width1/length1/price1;
 
             Thread.Sleep(5000);
             Clear = (Color.White);
@@ -103,16 +106,34 @@ namespace Variables
 
         private void button4_Click(object sender, EventArgs e)
         {
+            // Bill
+        
             button1.Visible = false;
             button2.Visible = false;
             button3.Visible = false;
             button4.Visible = false;
 
-            firstName = "Wayne Gretzky";
-            outputLabel.Text = firstName;
-            int numberName = 99;
-            outputLabel.Text = firstName + "is number" + numbername + ".";
-
+            string.change1 = "Change";
+            string.money1 = "Money";
+            string.price1 = "Price";
+            string.taxes1 = "Taxes";
+            string.preTaxes = "Original Cost";
+            int.money2 = "20";
+            int.price2 = "12.49";
+            int.tax = "0.13";
+            int.money/price = "7.51";
+            int.actualTax = "1.62";
+            int.pre-taxes = "10.87";
+            string.outputLabel.Text = change1 + "=" + "$" + money1 + "-" + price1;
+            string.outputLabel.Text = n / change1 + "=" + "$" + money2 + " - " + "$" + price2;
+            string.outputLabel.Text = n / change1 + "=" + "$" + money/price;
+            string.outputLabel.Text = n / ;
+            string.outputLabel.Text = n / preTaxes + "=" + price1 + "-" + "($" + price1 + " x " + taxes1 + ")";
+            string.outputLabel.Text = n / preTaxes + "=" + price2 + "-" + "($" + price2 + " x " + tax + ")";
+            string.outputLabel.Text = n / preTaxes + "=" + price2 + "-" + actualTax;
+            string.outputLabel.Text = n / preTaxes + "=" + pre-taxes;
+            string.outputLabel.Text = n / "Therefore, the " + preTaxes + "is " + pre-taxes + ", and the " + change1 + "is " money/price + ".";
+            
             Thread.Sleep(5000);
             Clear = (Color.White);
 
